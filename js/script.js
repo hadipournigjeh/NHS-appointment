2a42f631e19af443961807968d5151ccb83c85db
Karla Grajales <k.grajalesluna@gmail.com>
2023-01-24 13:24:51 +0000

    updated form and script

--- js/script.js
+++ js/script.js
@@ -2,24 +2,31 @@ const noButton = document.querySelector(".no");
 const textBox = document.querySelector(".text-box-1");
 
 noButton.addEventListener("click", function() {
+    textBox.removeAttribute("required");
     textBox.classList.add("hide");
 })
 
 const yesButton = document.querySelector(".yes");
 
 yesButton.addEventListener("click", function(){
-    textBox.classList.remove("hide")
+    textBox.classList.remove("hide");
+    textBox.setAttribute("required", true);
+
+    console.log("i am here");
 })
 
-const no2Button = document.querySelector(".no2");
-const textBox2 = document.querySelector(".text-box-2");
 
-no2Button.addEventListener("click", function() {
-    textBox2.classList.add("hide");
-})
+// const no2Button = document.querySelector(".no2");
+// const textBox2 = document.querySelector(".text-box-2");
 
-const yes2Button = document.querySelector(".yes2");
+// no2Button.addEventListener("click", function() {
+//     textBox2.classList.add("hide");
+// })
 
-yes2Button.addEventListener("click", function(){
-    textBox2.classList.remove("hide")
-})
+// const yes2Button = document.querySelector(".yes2");
+
+// yes2Button.addEventListener("click", function(){
+//     textBox2.classList.remove("hide");
+
+// })
+
